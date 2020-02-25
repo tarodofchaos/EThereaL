8ee75bfe1dcefccb447605e8e6f89cc19471705e
Daniel Gutierrez <dgutierrezsaavedra@gmail.com>
2020-02-25 14:56:00 +0100

    Gherkin tests FTW!!

--- EThereaL/src/test/java/org/chaos/ethereal/cucumber/ArmyCreator.java
+++ EThereaL/src/test/java/org/chaos/ethereal/cucumber/ArmyCreator.java
@@ -3,9 +3,17 @@ package org.chaos.ethereal.cucumber;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.chaos.ethereal.TestContext;
 import org.chaos.ethereal.helper.ArmyHelper;
+import org.chaos.ethereal.helper.SetOfValuesHelper;
+import org.chaos.ethereal.helper.SpecsHelper;
 import org.chaos.ethereal.persistence.Army;
+import org.chaos.ethereal.persistence.Hero;
+import org.chaos.ethereal.persistence.Monster;
+import org.chaos.ethereal.persistence.SetOfValues;
+import org.chaos.ethereal.persistence.Specs;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -19,12 +27,60 @@ public class ArmyCreator {
 		static Army generateAnArmy(Integer monstersSize, Integer heroesSize) throws Exception {
 			return helper.createArmy(monstersSize, heroesSize);
 		}
+		
+		static Boolean validateArmy(Object hm){
+			SpecsHelper specsHelper = new SpecsHelper();
+			SetOfValuesHelper setOfValuesHelper = new SetOfValuesHelper();
+			List<Specs> specs = specsHelper.retrieveAllFileSpecs(new TestContext().getLogger());
+			List<SetOfValues> setOfValues = setOfValuesHelper.retrieveAllPossibleValues(new TestContext().getLogger());
+			try {
+				return helper.validateHeroMonster(hm, specs, setOfValues);
+			} catch (Exception e) {
+				return false;
+			}
+		}
 	}
 
 	Integer heroArmySize;
 	Integer monsterArmySize;
 	Army army;
 	String actualErrorMessage;
+	Integer goodDexValue;
+	Integer badDexValue;
+	Boolean validationResult;
+	Hero validatedHero;
+	Monster validatedMonster;
+	Object validatedHM;
+	
+	@Given("I have a valid Hero")
+	public void i_have_a_valid_Hero() {
+		validatedHero = new Hero();
+		validatedHero.setClazz("Fighter");
+		validatedHero.setDamage(12);
+		validatedHero.setDexterity(12);
+		validatedHero.setGender("Female");
+		validatedHero.setHitpoints(30);
+		validatedHero.setId(1);
+		validatedHero.setMagic(12);
+		validatedHero.setMana(30);
+		validatedHero.setName("Lydia");
+		validatedHero.setRace("Elf");
+		validatedHero.setStrength(12);
+	}
+	
+	@Given("a valid Monster")
+	public void a_valid_Monster() {
+		validatedMonster = new Monster();
+		validatedMonster.setArmor("1d6");
+		validatedMonster.setArmyId(1);
+		validatedMonster.setComputedHP(200);
+		validatedMonster.setHitpoints("4d4");
+		validatedMonster.setId(1);
+		validatedMonster.setLevel("2");
+		validatedMonster.setMainAttack("1d10");
+		validatedMonster.setName("Grog");
+		validatedMonster.setSpecialAttack("2d4");
+	}
 	
 	@Given("I generate an army from an API call")
 	public void i_generate_an_army_from_an_API_call() {
@@ -80,4 +136,49 @@ public class ArmyCreator {
 		
 	}
 	
+	@Given("a hero has a {int} as dex")
+	public void a_hero_has_a_as_dex(Integer int1) {
+		validatedHero = new Hero();
+	    validatedHero.setDexterity(int1);
+	}
+	
+	@When("the army validator is invoked")
+	public void the_army_validator_is_invoked() {
+		validationResult = ArmyCreatorTest.validateArmy(validatedHM);
+	}
+	
+	@Then("I should get a false value")
+	public void i_should_get_a_false_value() {
+	    assertTrue(!validationResult);
+	}
+	
+	@Given("a hero has a {string} as race")
+	public void a_hero_has_a_as_race(String string) {
+		validatedHero.setRace(string);
+	}
+	
+	@Given("a hero has a {string} as class")
+	public void a_hero_has_a_as_class(String string) {
+		validatedHero.setClazz(string);
+	}
+	
+	@Given("a monster has a {string} as hitpoints")
+	public void a_monster_has_a_as_hitpoints(String string) {
+		validatedMonster.setHitpoints(string);
+	}
+	
+	@Given("a monster has a {string} as level")
+	public void a_monster_has_a_as_level(String string) {
+	   validatedMonster.setLevel(string);
+	}
+	
+	@Given("a monster has a {string} as name")
+	public void a_monster_has_a_as_name(String string) {
+	   validatedMonster.setName(string);
+	}
+	
+	@Then("I should get a true value")
+	public void i_should_get_a_true_value() {
+		assertTrue(validationResult);
+	}
 }
--- EThereaL/src/test/resources/features/ArmyCreator.feature
+++ EThereaL/src/test/resources/features/ArmyCreator.feature
@@ -1,4 +1,12 @@
-Feature: Army creation via API Gateway
+Feature: Army creation and validation
+
+    It involves all the scenarios of creating a validating an Army
+    Consisting in a List of Hero and a List of Monster.
+    This covers all the Transform steps for a process. 
+
+  Background:
+    Given I have a valid Hero
+    And a valid Monster
 
   Scenario: Generate an army with fifty heroes and a thousand monsters
     Given I generate an army from an API call
@@ -16,3 +24,26 @@ Feature: Army creation via API Gateway
     Given I generate an army from an API call with invalid parameters
     When the army creator is called
     Then I should get an Exception with the error message
+    
+  Scenario Outline: Validation of an army
+    Given I generate an army from an API call
+    And a <army> has a <value> as <attribute>
+    When the army validator is invoked
+    Then I should get a <boolean> value
+
+    Examples:
+    | army    |                                                            value                                       | attribute | boolean |
+    |    hero |                                                                                                    100 |       dex |   false |
+    |    hero |          "Several Species of Small Furry Animals Gathered Together in a Cave and Grooving with a Pict" |      race |   false |
+    |    hero |                                                                                             "Musician" |     class |   false |
+    | monster |                                                                                              "4ad5+12" | hitpoints |   false |
+    | monster |                                                                                                  "300" |     level |   false |
+    | monster | "The Attack Of The Wrath Of The War Of The Death Of The Strike Of The Sword Of The Blood Of The Beast" |      name |   false |
+    |    hero |                                                                                                     20 |       dex |    true |
+    |    hero |                                                                                                  "Elf" |      race |    true |
+    |    hero |                                                                                              "Fighter" |     class |    true |
+    | monster |                                                                                               "4d5+12" | hitpoints |    true |
+    | monster |                                                                                                   "10" |     level |    true |
+    | monster |                                                                                            "The Beast" |      name |    true |
+     
+     
